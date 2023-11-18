733d6146e3499b3ee96f8eb9bcc20a4c04ec8500
Saugat-Shrestha <Shresthaasaugat31@gmail.com>
2023-11-18 09:50:50 +0545

    bus reservation with spring boot

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const busreservation());
+}
+
+class busreservation extends StatelessWidget {
+  const busreservation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Bus Reservation",
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+        brightness: Brightness.dark,
+      ),
+    );
+  }
+}
---
+++ lib/models/response_model.dart
@@ -0,0 +1,15 @@
+import 'package:bus_reservation/utils/constants.dart';
+
+class ResponseModel {
+  ResponseStatus responseStatus;
+  int statusCode;
+  String message;
+  Map<String, dynamic> object;
+
+  ResponseModel({
+    required this.responseStatus,
+    required this.statusCode,
+    required this.message,
+    required this.object,
+  });
+}
